46c4f9cad22f22b88ad2027aae3d55b5f5834b4d
gh28942 <GerH@example.com>
2021-03-08 18:36:15 +0100

    Updates

---
+++ precache-manifest.8756449b8bf4d0258cc511546b7585d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35276be52d7ad6017fa7cbfa89cd762d",
+    "url": "/dcb-simulator/index.html"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/dcb-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "71295de2cb13561adfca",
+    "url": "/dcb-simulator/static/css/main.7a703c32.chunk.css"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/dcb-simulator/static/js/2.effe3d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dcb-simulator/static/js/2.effe3d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71295de2cb13561adfca",
+    "url": "/dcb-simulator/static/js/main.8a062965.chunk.js"
+  },
+  {
+    "revision": "2cbf117dfa5819edc21a",
+    "url": "/dcb-simulator/static/js/runtime-main.4e918012.js"
+  }
+]);
